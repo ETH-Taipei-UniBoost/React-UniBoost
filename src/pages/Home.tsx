9e532c30267b979c60a4c368197263e0d7534e3b
roto93 <roto93@yahoo.com.tw>
2023-04-23 11:24:59 +0800

    :pencil: update wording

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,7 @@ const Home = () => {
       <Box textAlign={'center'}>
         <Heading fontSize={64} fontWeight={700} lineHeight={'77px'} mb={4}>UniBoost</Heading>
         <Text color="#888888" fontSize={24} lineHeight={'28px'} w={500} mb={10} fontWeight={300}>
-          A permissionless liquidity boosting protocol specifically for <span style={{ color: '#319795', fontWeight: 600 }}>Uniswap V2 and V3</span> with insurance protection.
+          A permissionless liquidity boosting protocol specifically for <span style={{ color: '#319795', fontWeight: 600 }}>Uniswap V3</span> with insurance protection.
         </Text>
         <ButtonGroup spacing={4}>
           <Link href='https://github.com/ETH-Taipei-UniBoost' target='_blank'>
